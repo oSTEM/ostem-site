631b70b7d8004c2facbe9f38df7402696528fe5e
cmytelka <claire.mytelka@gmail.com>
2021-04-22 17:23:35 -0400

    Nav link

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -54,6 +54,7 @@ const NavMenu = ({ open }: { open: boolean }) => {
         <NavItem href={"/"}>Conference Home</NavItem>
         <NavItem href={"/cfp"}>Call for Proposals</NavItem>
         <NavItem href={"https://ostem.org/"}>About oSTEM</NavItem>
+        <NavItem href={"/resources"}>Community Resources</NavItem>
       </ul>
       <style
         // we use custom css here since tailwind doesn't mix very well with data-* attributes
