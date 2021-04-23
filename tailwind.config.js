b21b06c98b237b0025ee71538ce97d08f49b417c
Travis DePrato <travisdeprato@gmail.com>
2021-04-23 13:01:10 -0700

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{ts,tsx,js,jsx,mdx}", "./src/pages/**/*.mdx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      flex: {
+        // add the flex-2 class
+        2: "2 2 0%",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
