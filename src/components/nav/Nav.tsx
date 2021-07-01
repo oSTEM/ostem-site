824051ccdac083278d645cff3280e48dee872227
Claire Mytelka <70677842+cmytelka@users.noreply.github.com>
2021-07-01 17:36:27 -0400

    Messing around with resources navigation (#31)
    * Messing around with resources navigation
    
    * Co-authored-by: Travis DePrato <travigd@users.noreply.github.com>
    
    Co-authored-by: cmytelka <claire.mytelka@gmail.com>

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -7,86 +7,21 @@ import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
 import { Breakpoint, Color, Font, TextSize } from "@/theme";
 import { UnstyledButton } from "../button";
 
+
 export const Nav = () => {
-  // When in mobile mode, control whether or not the menu is active
-  const [open, setOpen] = React.useState(false);
-  const toggleOpen = React.useCallback((e: React.SyntheticEvent) => {
-    e.preventDefault();
-    setOpen((open) => !open);
-  }, []);
-
   return (
-    <nav>
-      <div className="mb-8 flex items-center">
-        <NavToggle toggleOpen={toggleOpen} />
-        <img
-          alt="oSTEM logo"
-          src="/images/logo-banner.png"
-          className="block max-h-16 ml-auto md:mr-auto"
-        />
-      </div>
-      <NavMenu open={open} />
-    </nav>
-  );
-};
-
-const NavToggle = ({
-  toggleOpen,
-}: {
-  toggleOpen: (event: React.MouseEvent<HTMLButtonElement>) => void;
-}) => {
-  return (
-    <div className="text-2xl p-4 md:hidden">
-      <UnstyledButton onClick={toggleOpen} aria-label={"toggle navigation"}>
-        <FontAwesomeIcon className={`icon`} icon={faBars} role={"button"} />
-      </UnstyledButton>
-    </div>
-  );
-};
-
-const NavMenu = ({ open }: { open: boolean }) => {
-  return (
-    <div
-      className="nav-list" // used to control open/closed for mobile nav
-      data-nav-open={open}
+    <nav
+    
     >
-      <ul>
-        <NavItem href={"/"}>Conference Home</NavItem>
-        <NavItem href={"/cfp"}>Call for Proposals</NavItem>
-        <NavItem href={"https://ostem.org/"}>About oSTEM</NavItem>
+      <ul className="flex flex-col text-center list-none w-full">
+        <NavItem href={"/blm-resources"}>Black Lives Matter Resources</NavItem>
+        <NavItem href={"/crisis-resources"}>Crisis Resources</NavItem>
         <NavItem href={"/covid-resources"}>COVID-19 Resources</NavItem>
         <NavItem href={"/member-resources"}>Member Resources</NavItem>
-
+        <NavItem href={"/affinity-groups"}>Affinity Groups</NavItem>
+        <NavItem href={"/grad-app-assistance"}>Grad App Assistance</NavItem>
       </ul>
-      <style
-        // we use custom css here since tailwind doesn't mix very well with data-* attributes
-        jsx
-      >{`
-        .nav-list {
-          display: none;
-        }
-
-        .nav-list[data-nav-open="true"] {
-          display: block;
-        }
-
-        .nav-list ul {
-          display: flex;
-          flex-flow: column nowrap;
-          text-align: center;
-          list-style: none;
-          width: 100%;
-        }
-
-        @media only screen and (min-width: ${Breakpoint.Sm}) {
-          .nav-list {
-            padding: 1rem;
-            width: 12rem;
-            display: flex;
-          }
-        }
-      `}</style>
-    </div>
+    </nav>
   );
 };
 
